69b7e8943b240f23770b656e16c0f1cd58ca42c0
Lautaro Lasorsa <llasorsa@alumno.unlam.edu.ar>
2024-08-18 00:37:20 -0300

    Crear notebook.tex
    Creo el archivo .tex donde escribiré el notebook.

--- utilitarios/run.sh
+++ utilitarios/run.sh
@@ -1,1 +1,7 @@
 cp $1.py $1.print; for x in $1*.in; do echo ARCHIVO: $x; cat $x; echo ===; python3 $1.py<$x; echo ===; done | tee -a $1.print
+
+# Uso: ./run.sh nombre_programa
+# Notar que no ponemos nombre_programa.py, sino solo nombre programa
+# Importante: Los casos de prueba deben estar en el mismo directorio que el programa
+# Los archivos de entrada deben tener la extensión .in
+# Ej: ./run.sh A para ejecutar el programa A.py con los casos de prueba A1.in, A2.in, etc.
